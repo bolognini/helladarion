c3419bcc652c3d44d8111faf8022c1137f2b2b14
bolognini <matheusbolognini79@gmail.com>
2020-12-31 09:34:01 -0300

    feat: add admin key

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,22 @@
 import express from 'express'
 import routes from './routes'
 import cors from 'cors'
-import firebase from 'firebase'
+import * as admin from "firebase-admin"
+import serviceAccount from './serviceAccountKey.json'
 
 const app = express()
 
-export const fireData = firebase.initializeApp({
-  apiKey: "AIzaSyD7n5thEFnMsCWOamdA-JCzP3q7hernoJc",
-  authDomain: "t20-monster-builder.firebaseapp.com",
-  projectId: "t20-monster-builder",
-  storageBucket: "t20-monster-builder.appspot.com",
-  messagingSenderId: "1086763486433",
-  appId: "1:1086763486433:web:4552ad0b532f4d403dbba0"
+const fireBase = admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://t20-monster-builder-default-rtdb.firebaseio.com"
 })
 
 export const saveMonster = (monster: any, id: string) => {
-  return fireData.database().ref('monsters/' + id).set(monster)
+  return fireBase.database().ref('monsters/' + id).set(monster)
 }
 
 export const getMonster = (id: string) => {
-  return fireData.database().ref('monsters/' + id).once("value")
+  return fireBase.database().ref('monsters/' + id).once("value")
 }
 
 app.use(express.json())
