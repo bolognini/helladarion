cb8f54670715cb78b8c9134a79b3e792af985fb8
bolognini <matheusbolognini79@gmail.com>
2021-04-22 14:55:31 -0300

    chore: add heroku configuration

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,25 @@
+import dotenv from 'dotenv'
 import express from 'express'
 import routes from './routes'
 import cors from 'cors'
 import * as admin from "firebase-admin"
-import serviceAccount from '../../serviceAccountKey.json'
 
+dotenv.config()
 const app = express()
 
 const fireBase = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    "type": process.env.FIREBASE_TYPE,
+    "project_id": process.env.FIREBASE_PROJECT_ID,
+    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+    "private_key": process.env.FIREBASE_PRIVATE_KEY,
+    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+    "client_id": process.env.FIREBASE_CLIENT_ID,
+    "auth_uri": process.env.FIREBASE_AUTH_URI,
+    "token_uri": process.env.FIREBASE_TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL
+  }),
   databaseURL: "https://t20-monster-builder-default-rtdb.firebaseio.com"
 })
 
@@ -24,7 +36,7 @@ export const getMonster = (id: string) => {
 }
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({ origin: ['https://helladarion-codex.netlify.app/', 'https://helladarion-form.netlify.app/'] }))
 app.use(routes)
 
 app.listen(3333)
