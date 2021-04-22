79cfe8fdef91268109742d590a44d089334185bd
bolognini <matheusbolognini79@gmail.com>
2021-04-22 15:38:13 -0300

    fix: add ts-node

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: yarn dev:server
+web:ts-node src/index.ts
--- src/index.ts
+++ src/index.ts
@@ -3,23 +3,26 @@ import express from 'express'
 import routes from './routes'
 import cors from 'cors'
 import * as admin from "firebase-admin"
+import { ServiceAccount } from "firebase-admin";
 
 dotenv.config()
 const app = express()
 
+const adminConfig = {
+  "type": process.env.FIREBASE_TYPE,
+  "project_id": process.env.FIREBASE_PROJECT_ID,
+  "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+  "private_key": process.env.FIREBASE_PRIVATE_KEY,
+  "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+  "client_id": process.env.FIREBASE_CLIENT_ID,
+  "auth_uri": process.env.FIREBASE_AUTH_URI,
+  "token_uri": process.env.FIREBASE_TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL
+}
+
 const fireBase = admin.initializeApp({
-  credential: admin.credential.cert({
-    "type": process.env.FIREBASE_TYPE,
-    "project_id": process.env.FIREBASE_PROJECT_ID,
-    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-    "private_key": process.env.FIREBASE_PRIVATE_KEY,
-    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-    "client_id": process.env.FIREBASE_CLIENT_ID,
-    "auth_uri": process.env.FIREBASE_AUTH_URI,
-    "token_uri": process.env.FIREBASE_TOKEN_URI,
-    "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-    "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL
-  }),
+  credential: admin.credential.cert(adminConfig as ServiceAccount),
   databaseURL: "https://t20-monster-builder-default-rtdb.firebaseio.com"
 })
 
