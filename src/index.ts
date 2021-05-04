1905f73c976cabb929d735ccbc167df14b6d3d2b
bolognini <matheusbolognini79@gmail.com>
2021-05-04 15:10:50 -0300

    feat: update limit for getting bigger images

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import { ServiceAccount } from "firebase-admin";
 dotenv.config()
 const app = express()
 
-app.use(express.json({ limit: '25mb' }))
+app.use(express.json({ limit: '50mb' }))
 
 app.use(express.json())
 app.use(cors({
