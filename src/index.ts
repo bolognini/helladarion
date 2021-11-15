6c600fc32fecb758b100ace528a5c277f2d9feed
Felipe Caminada <caminadaf@gmail.com>
2021-11-14 18:08:37 -0800

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,23 @@
-import dotenv from 'dotenv'
-import express from 'express'
-import routes from './routes'
-import cors from 'cors'
-import * as admin from "firebase-admin"
-import { ServiceAccount } from "firebase-admin";
+import dotenv from 'dotenv';
+import express from 'express';
+import routes from './routes';
+import cors from 'cors';
+import * as admin from "firebase-admin";
 
-dotenv.config()
-const app = express()
+dotenv.config();
+const app = express();
 
-app.use(express.json({ limit: '50mb' }))
+app.use(express.json({ limit: '50mb' }));
 
-app.use(express.json())
+app.use(express.json());
 app.use(cors({
   origin: '*',
   allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept,Authorization',
   methods: ['GET', 'PUT', 'POST']
-}))
-app.use(routes)
+}));
+app.use(routes);
 
-const privateKey: string = process.env.FIREBASE_PRIVATE_KEY as string
-
+const privateKey: string = process.env.FIREBASE_PRIVATE_KEY as string;
 const adminConfig = {
   "type": process.env.FIREBASE_TYPE,
   "project_id": process.env.FIREBASE_PROJECT_ID,
@@ -31,23 +29,30 @@ const adminConfig = {
   "token_uri": process.env.FIREBASE_TOKEN_URI,
   "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
   "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL
-}
+};
 
-const fireBase = admin.initializeApp({
-  credential: admin.credential.cert(adminConfig as ServiceAccount),
-  databaseURL: "https://t20-monster-builder-default-rtdb.firebaseio.com"
-})
+const firebase = admin.initializeApp({
+  credential: admin.credential.cert(adminConfig as admin.ServiceAccount),
+  databaseURL: "https://gerenciador-rpg-default-rtdb.firebaseio.com/"
+});
+
+// The app only has access as defined in the Security Rules
+var db = admin.database(firebase);
 
 export const createMonster = (monster: any, id: string) => {
-  return fireBase.database().ref('monsters/' + id).set(monster)
+  return db.ref('monsters/' + id).set(monster);
 }
 
 export const updateMonster = (monster: any, id: string) => {
-  return fireBase.database().ref('monsters/' + id).set(monster)
+  return db.ref('monsters/' + id).set(monster);
 }
 
 export const getMonster = (id: string) => {
-  return fireBase.database().ref('monsters/' + id).once("value")
+  return db.ref('monsters/' + id).once("value");
 }
 
-app.listen(process.env.PORT || 3333)
+export const listMonsters = () => {
+  return db.ref('monsters').once("value");
+}
+
+app.listen(process.env.PORT || 3333);
